edfba6486e2fb881b0d564f8f4fc927e102aab93
TreshMiralissa <muratet.jade@yahoo.com>
2022-11-25 11:39:16 +0100

    error correction

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -6,19 +6,20 @@ function App() {
 
 const apiKey = "fbbdfa582896093b8da1f248bec231ee"
 const [weatherData, setWeatherData] = useState({})
-const [city, setCity] = useState()
-const getWeather = (event) =>{
-  if(event.key == "Enter"){
-    fetch("api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}")
+const [city, setCity] = useState({})
+
+const getWeather = (e) =>{
+  if(e.key == "Enter"){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`)
     .then (response => response.json())
-    .then (data => {setWeatherData(data), setCity()})
+    .then (data => {setWeatherData(data), setCity("")})
   }
 }
 
   return(
     <div className="container">
       <h2 className="m-10">What is the weather like in ... ?</h2>
-      <input type="text" className="input" onChange={e => setCity (e.target.value)} value={city} onKeyPress={getWeather} placeholder="Search a city" />
+      <input className="input" onChange={e => setCity (e.target.value)} onKeyPress={getWeather} placeholder="Search a city" />
       {typeof weatherData.main === "undefined"?(
         <div>
           <p>Please enter a city</p>
@@ -26,7 +27,8 @@ const getWeather = (event) =>{
       (<div>
         <p>{weatherData.name}</p> {/*display name of city*/}
         <p>{Math.round (weatherData.main.temp)}°C</p> {/*display temperature*/}
-        <p>{weatherData.weather.main}</p> {/*display weather */}
+        <p>{weatherData.weather[0].main}</p> {/*display weather */}
+        <p>{Math.round (weatherData.main.humidity)}%</p> 
       </div>
       )}
     </div>
