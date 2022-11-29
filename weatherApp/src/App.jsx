319c4abfd895ff68f648b025010b6fd7293019f4
TreshMiralissa <muratet.jade@yahoo.com>
2022-11-29 10:15:54 +0100

    display five next days

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -9,6 +9,7 @@ const apiKey = "fbbdfa582896093b8da1f248bec231ee"
 const [weatherData, setWeatherData] = useState({})
 const [city, setCity] = useState({})
 const [nextDays, setNextDays] = useState({})
+const [forecastArray, setForecastArray] = useState ([])
 
 const handleClick = (e) => {
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`) /*weather of the day*/
@@ -18,11 +19,11 @@ const handleClick = (e) => {
 
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${apiKey}`) /*weather forecast of the five next days*/
   .then (response => response.json())
-  .then (data => {setNextDays(data)})
+  .then (data => {setNextDays(data), setForecastArray([data.list[8],data.list[16],data.list[24],data.list[32],data.list[39]])})
+
+  
 };
-
-
-
+// console.log(forecastArray);
 
 const getWeather = (e) =>{
   if(e.key == "Enter"){
@@ -39,7 +40,7 @@ const getWeather = (e) =>{
       </div>
       {typeof weatherData.main === "undefined"?(
         <div>
-          <p>Please enter a city</p>
+          <p></p>
         </div>):
               ( <div>
           <div>
@@ -49,13 +50,18 @@ const getWeather = (e) =>{
             <p>{weatherData.weather[0].main}</p> {/*display weather */}
             <p>{Math.round (weatherData.main.humidity)}%</p> 
           </div>
-          {/* <div>
+          <div>
             <p>Next days</p>
-            <p>{nextDays}</p>
-            <p>{Math.round (weatherData.main.temp)}°C</p> 
-            <p>{weatherData.weather[0].main}</p>
-            <p>{Math.round (weatherData.main.humidity)}%</p> 
-          </div> */}
+            <ul>
+              {forecastArray?.map((day)=>
+              <li key="">
+                <p>{day.dt_txt}</p>
+                <p>{Math.round (day.main.temp)}°C</p> 
+                <p>{day.weather[0].main}</p>
+                <p>{Math.round (day.main.humidity)}%</p>
+              </li>)}
+            </ul>
+          </div>
         </div>
       )}
      
