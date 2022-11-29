bfc23df215325646542af8d8dc6ae1872ccfd991
TreshMiralissa <muratet.jade@yahoo.com>
2022-11-29 17:53:32 +0100

    style correction

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -18,9 +18,12 @@ const [forecastArray, setForecastArray] = useState ([])
 
 
 const handleClick = (e) => {
-  fetch(`https://unsplash.com/photo/random/query=${city}?client_id=${unsplachKey}`) /*display image*/
+  let randomNumber = Math.floor(Math.random() * 10);
+  fetch(`https://unsplash.com/search/photo?query=${city}?client_id=${unsplachKey}`) /*display image*/
   .then (response => response.json())
-  .then (data => {setCityImage(data)})
+  .then (data => { let allImages = data.results[randomNumber];
+    return allImages.urls.regular})
+
 
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`) /*weather of the day*/
   .then (response => response.json())
@@ -40,9 +43,9 @@ const getWeather = (e) =>{
 }
 
   return(
-    <div  className="container w-fit mx-3 justify-center md:mx-10">
+    <div  className="container ">
       <h2 className="mt-10 mb-5">What is the weather like in ... ?</h2>
-      <div className="flex flex-wrap w-fit p-2 bg-gradient-to-r from-pink-500 to-yellow-500">
+      <div className="flex flex-wrap w-fit p-2 rounded bg-gradient-to-r from-pink-500 to-yellow-500">
         <input className="input pl-2" ref={ref} onChange={e => setCity (e.target.value)} onKeyPress={getWeather} placeholder="Search a city" />
         <button className=" p-2" onClick={handleClick}>Search</button>
       </div>
@@ -50,31 +53,30 @@ const getWeather = (e) =>{
       {typeof weatherData.main === 'undefined'?(
         <p className="mt-2">Please enter a city</p>
       ):
-      (<div className= "w-fit">
-        <div className="max-w-max xl:ml-auto mx-auto">
+      (<div className= "bg-gradient-to-r from-cyan-500 to-blue-500 mt-10 rounded-lg shadow-lg px-4 py-4 xl:py-12 xl:px-28 md:px-12 md:py-8">
+        <div className="flex flex-col justify-center items-center xl:ml-auto mx-auto">
           <img src="" alt="" />
           <p className="mb-5 mt-12 text-xl xl:pl-4">Today in {weatherData.name}</p> 
             {/*display name of city*/}
-          <div className="flex mb-20 mt-12 flex-col justify-center text-xl ml-12 xl:pl-4 xl:border-l-2 xl:border-l-indigo-500">
+          <div className="m-6 p-3 flex flex-col justify-center items-center">
             <p>{Math.round (weatherData.main.temp)}°C</p>
             {/*display temperature*/}
             <p>{weatherData.weather[0].main}</p>
             {/*display weather */}
             <p>Humidity : {Math.round (weatherData.main.humidity)}%</p> 
             <p>{emojis.wind} Wind: {weatherData.wind.speed}km/h</p>
-            <p>{weatherData.sys.sunrise}</p> 
           </div>
         </div>
-      <div className="my-10">
-          <p  className="text-lg font-bold ml-6">Next days</p>
-          <ul className="text-center m-6 p-3 rounded-md bg-white/30 shadow-md flex flex-wrap justify-around items-center sm:flex-row">
+        <div className="my-10">
+          <p  className="text-lg font-bold">Next days</p>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-3 xl:grid-cols-5">
             {forecastArray?.map((day)=>
-              <li className="m-2">
+              <div className="text-center p-6 rounded-md bg-white/30 shadow-md flex justify-center items-center flex-col">
                 <p>{emojis.calendar}{Moment(day.dt_txt).format('MM-DD-YYYY')}</p>
                 <p>{Math.round (day.main.temp)}°C</p> 
                 <p>{day.weather[0].main}</p>
-              </li>)}
-          </ul>
+              </div>)}
+            </div>
           </div>
         </div>
       )}
